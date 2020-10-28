f7cc491de110118343d7d2e59ea374025b3f6450
minkyu070602 <minkyu070602@gmail.com>
2020-10-28 22:56:09 +0900

    Updates

---
+++ precache-manifest.b64fda164f3221e41a86e0d118176502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79a4449bb55243cc82af505c02209ed",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "94e7af678d7f318b66c9",
+    "url": "/nwitter/static/css/main.f4df1dc8.chunk.css"
+  },
+  {
+    "revision": "475291a1b7a4e718e7fd",
+    "url": "/nwitter/static/js/2.685a7341.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.685a7341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e7af678d7f318b66c9",
+    "url": "/nwitter/static/js/main.25d3e7f8.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
