f344ab6fad698dc0f7b96d4d331239029ee36f44
kinqq <minkyu070602@gmail.com>
2021-01-15 20:12:34 +0900

    Login Error Fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,48 @@
 import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [init, setInit] = useState(false);
-  const [userObj, setUserObj] = useState(null);
-  useEffect(() => {
-    authService.onAuthStateChanged((user) => {
-      if (user) {
-        setUserObj({
-          displayName: user.displayName,
-          uid: user.uid,
-          updateProfile: (args) => user.updateProfile(args),
+    const [init, setInit] = useState(false);
+    const [userObj, setUserObj] = useState(null);
+    useEffect(() => {
+        authService.onAuthStateChanged((user) => {
+            if (user) {
+                setUserObj({
+                    displayName: user.displayName,
+                    uid: user.uid,
+                    updateProfile: (args) => user.updateProfile(args),
+                });
+            } else {
+                setUserObj(null);
+            }
+            setInit(true);
         });
-      } else {
-        setUserObj(null);
-      }
-      setInit(true);
-    });
-  }, []);
-  const refreshUser = () => {
-    const user = authService.currentUser;
-    setUserObj({
-      displayName: user.displayName,
-      uid: user.uid,
-      updateProfile: (args) => user.updateProfile(args),
-    });
-  };
-  return (
-    <>
-      {init ? (
-        <AppRouter
-          refreshUser={refreshUser}
-          isLoggedIn={Boolean(userObj)}
-          userObj={userObj}
-        />
-      ) : (
-        "Initializing..."
-      )}
-    </>
-  );
+    }, []);
+    const refreshUser = () => {
+        const user = authService.currentUser;
+        setUserObj({
+            displayName: user.displayName,
+            uid: user.uid,
+            updateProfile: (args) => user.updateProfile(args),
+        });
+    };
+    return (
+        <>
+            {init ? (
+                <BrowserRouter>
+                    <AppRouter
+                        refreshUser={refreshUser}
+                        isLoggedIn={Boolean(userObj)}
+                        userObj={userObj}
+                    />
+                </BrowserRouter>
+            ) : (
+                "Initializing..."
+            )}
+        </>
+    );
 }
 
 export default App;
